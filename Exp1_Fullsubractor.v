b71755914566dd81a0632ec6c1752a8970d3c8a4
kavyavasudevan04 <160568722+kavyavasudevan04@users.noreply.github.com>
2024-04-12 10:11:12 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,25 @@
-module fs(a,b,bin,d,bout);
-input a,b,bin;
-output d,bout;
+module fs(a, b, c, borrow, diff);
 
+input a;
 
+input b;
+
+input c;
+
+output borrow;
+
+output diff;
+
+wire d,e,f;
+
+xor(diff,a,b,c);
+
+and(d,~a,b);
+
+and(e,b,c);
+
+and(f,~a,c);
+
+or(borrow,d,e,f);
 
 endmodule
